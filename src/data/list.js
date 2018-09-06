cd721b856393fd66ae94fa419d074536d7089075
Pangu <Pangu@DESKTOP-IF6DCCV>
2018-09-05 18:39:25 -0700

    completed displaying todoData

---
+++ src/data/list.js
@@ -0,0 +1,23 @@
+export default [
+    {
+        title: 'Wash Car',
+        details: 'Clean inside and outside',
+        id: '001'
+    },{
+        title: 'Wash Car2',
+        details: 'Clean IT',
+        id: '002'
+    },{
+        title: 'Wash Car3',
+        details: 'Clean IT NOW MAN',
+        id: '003'
+    },{
+        title: 'Wash Car4',
+        details: 'Still haven\'t cleaned it???',
+        id: '004'
+    },{
+        title: 'Wash Car5',
+        details: 'Just clean it....',
+        id: '005'
+    }
+];
