9536ae9f362c3d1a63746642db3eb38ae41cc6e9
Pangu <Pangu@DESKTOP-IF6DCCV>
2018-09-26 19:01:47 -0700

    added delete button functionality

--- src/components/item.js
+++ src/components/item.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
 export default (props) => {
+
     return (
         <li className="input-group input-group-sm mb-2">
         <div className="form-control">{props.title}</div>
         
         <div className="input-group-append">
-            <button type="button" className="btn btn-danger btn-sm" onClick={()=>{}}>Delete</button>
+            <button type="button" className="btn btn-danger btn-sm" onClick={props.delete}>Delete</button>
         </div>
     </li>
     )
--- src/components/list.js
+++ src/components/list.js
@@ -3,9 +3,11 @@ import Item from './item';
 
 
 export default (props) => {
+
         const listElements = props.data.map((item,index)=>{
-            console.log('Item:', item);
-            return <Item key={item._id} title={item.title} />
+            //you can pass in param into function so when it is called, you already have it preset.
+            return <Item key={item._id} title={item.title} delete={props.delete.bind(this, item._id)} />
+
         });
     
         //babel can unwrape array's.
